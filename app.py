8ddc2964481154a3e167b97d167e688d58d0a28b
gideon2109 <ntimgyakari.g@northeastern.edu>
2026-05-23 03:57:30 +0000

    Fix line 551 too long

--- app.py
+++ app.py
@@ -548,7 +548,9 @@ def auto_load_data():
 
             for row in reader:
                 if all(col in row for col in ['recordErrorID', 'recordFile', 'exampleExample', 'recordTime']):
-                    try:
+                required_cols = ["recordErrorID", "recordFile",
+                                 "exampleExample", "recordTime"]
+                if all(col in row for col in required_cols):
                         record_time = time_to_seconds(row['recordTime']) if row['recordTime'].strip() else 0.0
                         record_file = row['recordFile'].strip()
 
